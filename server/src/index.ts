c375a0c91e2e79f4fa63c5a46aff7cfbc65fab76
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2022-04-26 22:30:57 +0100

    add cors, compression middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import compression from "compression";
+import cors from "cors";
 import routes from "./api/routes";
 
 const app = express();
 const port = 3000;
+
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use("/api/", routes);
 
