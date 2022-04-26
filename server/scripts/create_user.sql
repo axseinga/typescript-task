b535ff080030ecd96262d9ea23d858d4b6d51904
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2022-04-26 21:20:56 +0100

    add mysql and create scripts for db, add ts config

--- server/scripts/create_user.sql
+++ server/scripts/create_user.sql
@@ -1,4 +1,4 @@
-CREATE USER 'users_db_api'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'users_db_api'@'localhost' IDENTIFIED BY 'my666%SecretPassword';
 
 GRANT ALL PRIVILEGES ON users_db.users TO 'users_db_api'@'localhost';
 
