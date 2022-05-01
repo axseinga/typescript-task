d9a8f7ff2fc484c0499c91870d525e22dc76abe0
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2022-05-01 23:20:29 +0100

    create basic table

--- client/src/pages/PlaygroundPage/PlaygroundPage.tsx
+++ client/src/pages/PlaygroundPage/PlaygroundPage.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
-import { StyledPlaygroundPage } from './styled/PlaygroundPage.styled';
+import { StyledPlaygroundPage, StyledUsersList } from './styled/PlaygroundPage.styled';
 import { Button } from '../../components/Button';
+import { UsersState } from '../../state/usersState';
+import { TiTick } from 'react-icons/ti';
+import { ImCross } from 'react-icons/im';
 
 export const PlaygroundPage = () => {
+  const { users, setUsers } = UsersState();
+
   return (
     <StyledPlaygroundPage>
       <div>
         <Button type='button' primary>
-          Show me users
+          Refresh list
         </Button>
+        <StyledUsersList>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>Name</th>
+              <th>Surname</th>
+              <th>e-mail</th>
+              <th>newsletter</th>
+              <th>Options</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users?.map(user => {
+              return (
+                <tr key={user.id}>
+                  <th>{user.id}</th>
+                  <th>{user.firstName}</th>
+                  <th>{user.lastName}</th>
+                  <th>{user.email}</th>
+                  <th>{user.newsletter ? <TiTick /> : <ImCross size={8} />}</th>
+                </tr>
+              );
+            })}
+          </tbody>
+        </StyledUsersList>
       </div>
     </StyledPlaygroundPage>
   );
--- client/src/pages/PlaygroundPage/styled/PlaygroundPage.styled.ts
+++ client/src/pages/PlaygroundPage/styled/PlaygroundPage.styled.ts
@@ -1,23 +1,41 @@
 import styled from 'styled-components';
 
 export const StyledPlaygroundPage = styled.section`
+  background-color: white;
+  padding: 2rem;
+  border-radius: 10px;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-direction: column;
+  min-width: 400px;
+  box-shadow: ${({ theme }) => theme.shadow.box};
+  height: 70vh;
+  width: 60vw;
 
   & > div {
-    background-color: white;
-    padding: 2rem;
-    border-radius: 10px;
-    display: flex;
-    flex-direction: column;
-    min-width: 400px;
-    box-shadow: ${({ theme }) => theme.shadow.box};
-    height: 70vh;
-    width: 60vw;
-
     & button {
       align-self: center;
+    }
+  }
+`;
+
+export const StyledUsersList = styled.table`
+  width: 100%;
+  border: 1px solid gray;
+
+  thead {
+    tr {
+      th {
+        border: 1px solid gray;
+        padding: 5px 10px;
+      }
+    }
+  }
+
+  tbody {
+    tr {
+      th {
+        padding: 0.5rem;
+      }
     }
   }
 `;
